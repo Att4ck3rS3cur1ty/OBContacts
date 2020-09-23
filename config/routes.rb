f2e0075e9bc16f5e7420e9489f7b23d611a6e3e8
kadson@elpescador.com.br <kadson@elpescador.com.br>
2020-09-23 16:48:12 -0300

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :contacts
+    end
+  end
+  
+  # this would generate something like: api.obcontacts.com/v1/contacts
+=begin   constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resources :contacts
+      end
+    end
+  end =end
+
 end
